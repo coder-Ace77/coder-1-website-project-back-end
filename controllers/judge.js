a1b841378071192b2a41cba66fdd864f7d0aa496
coder-Ace77 <adil7k777@gmail.com>
2024-02-27 19:27:18 +0530

    validte by code fn added

--- controllers/judge.js
+++ controllers/judge.js
@@ -9,8 +9,23 @@ const validate = (test_input, test_output, output_path, correct_ans) => {
     var child = cp.spawn(output_path, { stdio: [input_file, output_file] });
     child.on('exit', (code, signal) => {
         fs.readFile(test_output, 'utf8', (err, content) => {
-            console.log(`#${content}#`, `#${correct_ans}#`);
             if (content === correct_ans) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+    });
+}
+
+const validate_by_code = (test_input, test_output, output_path, input, validator) => {
+    const input_file = fs.openSync(test_input, "r");
+    const output_file = fs.openSync(test_output, "w");
+    var child = cp.spawn(output_path, { stdio: [input_file, output_file] });
+    child.on('exit', (code, signal) => {
+        fs.readFile(test_output, 'utf8', (err, content) => {
+            if (validator(input, content) == true) {
                 return true;
             }
             else {
@@ -34,26 +49,27 @@ exports.quesSubmitController = (req, res) => {
         const test_output = path.join('test_output', filename.split('.')[0] + '.txt');
         fs.writeFileSync(input_path, req.body.code);
         cp.exec(`g++ "${input_path}" -o "${output_path}"`, (err) => {
-            for (let i = 0; i < result.test_cases_count; i++) {
-                fs.writeFileSync(test_input, result.test_case_input[i]);
-                fs.writeFileSync(test_output, result.test_case_output[i]);
-                if (validate(test_input, test_output, output_path, result.test_case_output[i]) == false) {
-                    res.json({ status: `Wrong Answer on test case ${i + 1}` });
-                    return;
+            if (result.validate_by_code == false) {
+                for (let i = 0; i < result.test_cases_count; i++) {
+                    fs.writeFileSync(test_input, result.test_case_input[i]);
+                    fs.writeFileSync(test_output, result.test_case_output[i]);
+                    if (validate(test_input, test_output, output_path, result.test_case_output[i]) == false) {
+                        res.json({ status: `Wrong Answer on test case ${i + 1}` });
+                        return;
+                    }
                 }
+                res.json({ status: "Correct Answer" });
             }
-            res.json({ status: "Correct Answer" });
+            else {
+                for (let i = 0; i < result.test_cases_count; i++) {
+                    fs.writeFileSync(test_input, result.test_case_input[i]);
+                    if (validate_by_code(test_input, test_output, output_path, result.test_case_input[i], result.code) == false) {
+                        res.json({ status: `Wrong Answer on test case ${i + 1}` });
+                        return;
+                    }
+                }
+                res.json({ status: "Correct Answer" });
+            }
         });
-
-
-
-
-        // first write code file and then compile it
-
-        // for each test case write it and then run it and then compare the output
-
-        // if output is same then return correct answer else wrong answer
-
-
     });
 }
